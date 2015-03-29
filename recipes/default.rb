3091588677b008d986a61c2b39567fb67b0be042
Calvin So <calvin@outbid.com>
2015-03-29 07:28:42 -0700

    Added  libsqlite3-dev to the package installation list

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,7 +22,7 @@ mysql_root_password = "#{node[:mysql][:server_root_password]}"
 #
 # Installl other packages
 #
-%w{libssl-dev libmysqlclient-dev mysql-server-5.6 libreadline-dev}.each do |pkg|
+%w{libssl-dev libmysqlclient-dev mysql-server-5.6 libreadline-dev libsqlite3-dev}.each do |pkg|
   package pkg do
     action :install
   end
