0771aa7a22d4137fe60d05b86e4f59f3af48a9ee
socalvin <calvin@outbid.com>
2015-03-30 21:35:52 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Calvin So'
 maintainer_email 'calvin.so@non-exist.com'
 description      'A Chef cookbook provisioning all the basic software stack for a typical RoR developer.'
 long_description 'A Chef cookbook provisioning all the basic software stack for a typical RoR developer.'
-version          '1.0'
+version          '1.1'
 
 depends 'ntp'
 depends 'nginx'
